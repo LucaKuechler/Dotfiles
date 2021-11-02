16bd7875a30276433b097e700d0bc46650acb5c6
LucaKuechler <lucakuechler.business@gmail.com>
2021-11-02 18:55:30 +0100

    added autocompletion and tabnine support

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ## Install the Font
 https://github.com/pyrho/hack-font-ligature-nerd-font
+
+## Install unzip
+You need to install unzip to use unpack tabnine in vscode
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -52,6 +52,10 @@ return require('packer').startup(function()
      run = ':TSUpdate'
   }
 
+  -- auto completion
+  use "hrsh7th/nvim-cmp"
+  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+
   -- color theme
   use 'christianchiarulli/nvcode-color-schemes.vim'
 end)
