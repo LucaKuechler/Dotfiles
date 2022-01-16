6e5a1b73b3e99a2dd9398da24beee284ec949da8
LucaKuechler <lucakuechler.business@gmail.com>
2022-01-16 17:03:41 +0100

    added zoxide installation

--- auto_install.sh
+++ auto_install.sh
@@ -14,6 +14,7 @@ softwares=(
     "ranger"
     "starship"
     "tmux"
+    "zoxide"
     #"testcase"
 )
 
---
+++ scripts/.config/scripts/setup/ubuntu/install_zoxide.sh
@@ -0,0 +1,5 @@
+install_zoxide() {
+    curl -sS https://webinstall.dev/zoxide | bash
+}
+
+install_zoxide
