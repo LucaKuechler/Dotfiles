13d8e8e1bae375abd5ed5cff091d693cc930ccb7
Luca Kuechler <lucakuechler.business@gmail.com>
2022-05-20 19:02:06 +0200

    fix: set settings to global

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -8,10 +8,11 @@ wo.number = true -- file overall
 bo.smartindent = true -- indention
 bo.autoindent = true -- indention
 bo.expandtab = true -- convert tabs to spaces
-bo.tabstop = 4 -- Insert 4 spaces for tab
-bo.shiftwidth = 4 -- While tab=2 will always complete 1-2 this will complete 1-3
+o.tabstop = 4 -- Insert 4 spaces for tab
+o.shiftwidth = 4 -- While tab=2 will always complete 1-2 this will complete 1-3
 o.showtabline = 2 -- Always show tabs
 o.conceallevel = 0 -- `` in markdown
+
 
 o.splitbelow = true -- split window default
 o.splitright = true -- split window default
