5598c65cb06fabeaf67307682819c3044155ed12
LucaKuechler <lucakuechler.business@gmail.com>
2022-05-27 12:43:57 +0200

    few new things

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import subprocess
 from typing import List  # noqa: F401
 
@@ -7,80 +8,103 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+# CUSTOM
+from python.keymappings import keys
+from python.groups import groups
+#from python.screens import screens
+from python.layouts import layouts
+
 mod = "mod4"
-terminal = guess_terminal()
-
-keys = [
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod], "d", lazy.spawn("rofi -show drun"), desc="Spawn a command using a prompt widget"),
-]
-
-groups = [Group(i) for i in "123456789"]
 
 for i in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name)),
         ]
     )
 
-layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    font="Hack Nerd Font Mono",
+                    fontsize=30,
+                    #background="#1e1e2e99",
+                    background="#2C2C2C",
+                    margin_y=4,
+                    margin_x=5,
+                    padding_y=9,
+                    padding_x=4,
+                    borderwidth=7,
+                    inactive="#404040",
+                    active="#ffffff",
+                    rounded=True,
+                    highlight_color="#FFEA2E",
+                    highlight_method="text",
+                    this_current_screen_border="#FFEA2E",
+                    block_highlight_text_color="#1e1e2e",
+                ),
+                widget.Sep(
+                    padding=15,
+                    linewidth=0,
+                ),
+                widget.CurrentLayoutIcon(
+                    custom_icon_paths=[os.path.expanduser("~/.config/qtile/icons")],
+                    scale=0.6,
+                    background="#2C2C2C",
+                ),
+                widget.Spacer(),
+                widget.Systray(
+                    icon_size=20,
+                ),
+                widget.Sep(
+                    padding=15,
+                    linewidth=0,
+                ),
+                #widget.Volume(),
+                widget.CheckUpdates(
+                    background="#2C2C2C",
+                    foreground="#fae3b0",
+                    fontsize=18,
+                    update_interval=1800,
+                    distro='Manjaro',
+                    custom_command='pacman -Sup 2> /dev/null',
+                    no_update_string=' 📦 0  ',
+                    display_format=' 📦 {updates}  ',
+                ),
+                widget.Clock(
+                    background="#2C2C2C",
+                    format=" 🕑 %H:%M   📅 %d.%m.%Y", #format=" 📅 %d.%m.%Y [Week %W / %A]",
+                    timezone=datetime.timezone.utc,
+                    fontsize=18,
+                    padding=5,
+                ),
+                widget.Sep(
+                    padding=15,
+                    linewidth=0,
+                ),
+                widget.TextBox(
+                    text=" \uf303 ",
+                    fontsize=30,
+                    background="#2C2C2CAA",
+                    foreground="#fae3b0",
+                    padding=4,
+                ),
+            ],
+            34,
+            background="#00000000",
+            foreground="#1e1e2e",
+            margin=[10,6,8,10],
+        ),
+    ),
 ]
 
 widget_defaults = dict(
@@ -90,32 +114,33 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-    ),
-]
+# screens = [
+#     Screen(
+#         bottom=bar.Bar(
+#             [
+#                 widget.CurrentLayout(),
+#                 widget.GroupBox(),
+#                 widget.Prompt(),
+#                 widget.WindowName(),
+#                 widget.Chord(
+#                     chords_colors={
+#                         "launch": ("#ff0000", "#ffffff"),
+#                     },
+#                     name_transform=lambda name: name.upper(),
+#                 ),
+#                 widget.TextBox("default config", name="default"),
+#                 widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+#                 widget.Systray(),
+#                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+#                 widget.QuickExit(),
+#             ],
+#             24,
+#             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+#             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+#         ),
+#     ),
+# ]
+
 
 # Drag floating layouts.
 mouse = [
@@ -144,24 +169,11 @@ floating_layout = layout.Floating(
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-
-print("Hello")
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser('~/Dotfiles/qtile/.config/qtile/scripts/autostart.sh') # path to my script, under my user directory
     subprocess.call([home])
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
--- qtile/.config/qtile/scripts/autostart.sh
+++ qtile/.config/qtile/scripts/autostart.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
+betterlockscreen -w $HOME/Dotfiles/wallpaper/hacker.jpg
 picom --config $HOME/Dotfiles/picom/.config/picom/picom.conf &
 nitrogen --restore &
