a7873a91754d9677d9d2ec7427a92313911f90e5
LucaKuechler <lucakuechler.business@gmail.com>
2022-07-15 22:06:13 +0200

    remove warning for global vim

--- nvim/.config/nvim/lua/lsp/lsp-lua.lua
+++ nvim/.config/nvim/lua/lsp/lsp-lua.lua
@@ -4,4 +4,11 @@ require("lspconfig").sumneko_lua.setup({
 	on_attach = function(client, bufnr)
 		keymappings.keys(client, bufnr)
 	end,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
