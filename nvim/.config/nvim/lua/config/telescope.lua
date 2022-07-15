9f1bbaa7096d089385dd9b884e374903205345b7
LucaKuechler <lucakuechler.business@gmail.com>
2022-07-15 22:21:29 +0200

    fixed rg bug

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,9 +1,1 @@
-require("telescope").setup({
-	pickers = {
-		live_grep = {
-			additional_args = function(opts)
-				return { "--hidden", "--glob", "!.git" }
-			end,
-		},
-	},
-})
+require("telescope").setup({})
