989cdeff7495cefb9ac394edefe2ca24a9533de2
Luca Küchler <lucakuechler.business@gmail.com>
2023-06-11 14:25:22 +0200

    remove show buffers

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -10,7 +10,6 @@ bo.autoindent = true -- indention
 bo.expandtab = true -- convert tabs to spaces
 o.tabstop = 4 -- Insert 4 spaces for tab
 o.shiftwidth = 4 -- While tab=2 will always complete 1-2 this will complete 1-3
-o.showtabline = 2 -- Always show tabs
 o.conceallevel = 0 -- `` in markdown
 o.relativenumber = true --relative line numbers
 
