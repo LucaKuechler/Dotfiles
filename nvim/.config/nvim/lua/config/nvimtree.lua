87f108c16564bb488845c35ac8fe5be6280a4846
LucaKuechler <lucakuechler.business@gmail.com>
2024-06-03 11:53:14 +0200

    change keybinding for opening vertical split

--- nvim/.config/nvim/lua/config/nvimtree.lua
+++ nvim/.config/nvim/lua/config/nvimtree.lua
@@ -5,7 +5,7 @@ local function on_attach(bufnr)
 		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 	end
 
-	vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "<C-s>", api.node.open.vertical, opts("Open: Vertical Split"))
 	vim.keymap.set("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
 	vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
 	vim.keymap.set("n", "l", api.tree.reload, opts("Refresh"))
